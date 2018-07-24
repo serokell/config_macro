3bbe939ebb796bc78acbbb241dd87630cbb2949d
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-25 02:08:25 +0300

    Replace ConfigMacro.Option with normalize_param/1

--- lib/config_macro.ex
+++ lib/config_macro.ex
@@ -1,17 +1,19 @@
-alias ConfigMacro.Option
-
 defmodule ConfigMacro do
   defmacro __using__([]) do
     quote do: import unquote(__MODULE__)
   end
 
-  defmacro config(app, options) do
-    for option <- options do
+  defp normalize_param({k, v}), do: {k, v}
+  defp normalize_param(k) when is_atom(k), do: {k, nil}
+
+  defmacro config(app, params) do
+    for param <- params do
+      {key, value} = normalize_param(param)
       quote do
-        def unquote(Option.to_atom(option))() do
+        def unquote(key)() do
           unquote(app)
 	  |> Application.get_env(__MODULE__, [])
-	  |> Keyword.get(unquote_splicing(Option.to_list(option)))
+	  |> Keyword.get(unquote(key), unquote(value))
 	end
       end
     end
