8289bc05a026779aefbdd647c7c6e5dcbeb425f1
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-25 02:22:17 +0300

    Complete test suite

--- test/config_macro_test.exs
+++ test/config_macro_test.exs
@@ -2,9 +2,20 @@ defmodule ConfigMacroTest do
   use ConfigMacro
   use ExUnit.Case
 
-  config :config_macro, foo: 1
+  config __MODULE__, dakkon: 1, morte: 2
 
-  test "defines macro" do
-    assert foo() == 1
+  test "param returns default" do
+    assert dakkon() == 1
+  end
+
+  test "param can be overrided" do
+    Application.put_env(__MODULE__, __MODULE__, morte: 200)
+    assert morte() == 200
+  end
+
+  config __MODULE__, [:annah]
+
+  test "param w/o value returns nil" do
+    assert annah() == nil
   end
 end
