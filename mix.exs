60bece2ad7e53bffc86f4f5ceccedcf34b07b55c
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-25 02:45:10 +0300

    Prepare package for Hex

--- mix.exs
+++ mix.exs
@@ -4,6 +4,13 @@ defmodule ConfigMacro.MixProject do
   def project do
     [
       app: :config_macro,
+      description: "Macro that defines Application.get_env/{2,3} getters",
+      package: [
+        files: ["lib", "mix.exs"],
+	licenses: ["CC0-1.0"],
+	links: %{"GitLab" => "https://gitlab.com/serokell/config_macro"},
+	maintainers: ["Yegor Timoshenko"]
+      ],
       version: "1.0.0"
     ]
   end
