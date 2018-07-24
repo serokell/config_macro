42ca70c36b8e57c169da25c7d2ac726602514f90
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-25 02:45:57 +0300

    Downgrade version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule ConfigMacro.MixProject do
 	links: %{"GitLab" => "https://gitlab.com/serokell/config_macro"},
 	maintainers: ["Yegor Timoshenko"]
       ],
-      version: "1.0.0"
+      version: "0.1.0"
     ]
   end
 end
